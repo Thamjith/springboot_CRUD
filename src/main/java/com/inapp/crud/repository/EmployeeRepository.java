4c10b0d9a16207c7cf52ecf08024477e2f5765dc
Thamjith <jithuthaha8@gmail.com>
2022-11-28 13:29:02 +0530

    initial commit

---
+++ src/main/java/com/inapp/crud/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.inapp.crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.inapp.crud.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
---
+++ src/main/java/com/inapp/crud/service/EmployeeService.java
@@ -0,0 +1,43 @@
+package com.inapp.crud.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.inapp.crud.entity.Employee;
+import com.inapp.crud.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	public Employee save(Employee emp) {
+		return employeeRepository.save(emp);
+	}
+	
+	public Employee getEmployee(int id) {
+		return employeeRepository.findById(id).get();
+	}
+	
+	public Employee update(Employee employee) {
+		Employee emp = employeeRepository.findById(employee.getId()).get();
+		emp.setName(employee.getName());
+		emp.setAge(employee.getAge());
+		
+		return employeeRepository.save(emp);
+	}
+	
+	public String delete(int id) {
+		employeeRepository.deleteById(id);
+		return "Entity deleted =" + id;
+	}
+
+	public EmployeeRepository getEmployeeRepository() {
+		return employeeRepository;
+	}
+
+	public void setEmployeeRepository(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
+}
